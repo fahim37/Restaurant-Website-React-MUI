065aa14ac4bcb76b214d823f21c50c11d2bfce0a
Fahim Ahmed Emon <ahmed.fahim37@gmail.com>
2024-05-24 19:21:56 +0600

    first commit

---
+++ src/Pages/Pagenotfound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../Components/Layout/Layout'
+
+const Pagenotfound = () => {
+  return (
+    <Layout><h1>no page</h1></Layout>
+  )
+}
+
+export default Pagenotfound
