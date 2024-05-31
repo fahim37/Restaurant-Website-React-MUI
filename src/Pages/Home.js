4fdd20d80690cdb5a12b0377e6268eb47f56679d
Fahim Ahmed Emon <63098250+fahim37@users.noreply.github.com>
2024-05-31 22:25:51 +0600

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="home" style={{ backgroundImage: `url(${Banner})` }}>
         <div className="headerContainer">
           <h1>Indian Cusine</h1>
-          <p>Authentic Food in Bangladesh</p>
+          <p>Fresh Food</p>
           <Link to="/menu">
             <button>ORDER NOW</button>
           </Link>
